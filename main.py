eff3899df4e53042f9d1eb5a29b66703451b36f5
sugar-joh <jjohnsoncz233@gmail.com>
2024-11-20 19:56:22 -0800

    change main for new masses

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ sys.setrecursionlimit(100000000)
 
 
 # Constants
-number_particles = 100
+number_particles = np.array([250, 250])
 boundary_size = 1e-2
 init_method = 'plummer'
 mu = 0
 sigma_pos = 5e-3
 Temperature = 1e62
-particle_mass = 1e3
+particle_mass = np.array([1e3, 1e2])
 
 delta_t = 1000  # Time step in years that will be stored
 total_time = 1000000  # Total time in years
@@ -32,10 +32,11 @@ total_timestep = int(total_time / delta_t)  # Number of steps
 current_time = 0  # Current time in years
 tracking_timestep = 0  # Time interval to track when the positions will be saved
 
-particles_ini = Particles_ini(n=number_particles, boundary_size=boundary_size, init_method='plummer', mu=mu, sigma_pos=sigma_pos, Temperature=Temperature, mass=particle_mass, diff_mass=False) 
+particles_ini = Particles_ini(n=number_particles, boundary_size=boundary_size, init_method='plummer', mu=mu, sigma_pos=sigma_pos, Temperature=Temperature, mass=particle_mass) 
 position = particles_ini.position
 mass = particles_ini.mass
 velocity = particles_ini.velocity  # There is residual momentum in the each direction i.e. the total momentum is not 0
+number_particles = particles_ini.total_num
 acceleration = np.zeros((number_particles, 3))
 position_matrix = np.empty((number_particles, total_timestep, 3))  # Initialize position matrix to store positions at each time step
 
@@ -67,9 +68,9 @@ if init_method == 'plummer':
     fname = f'logT{np.log10(total_time):.1f}_N{number_particles}_logM{np.log10(particle_mass):.1f}_plummer.gif'
 elif init_method == 'boltzmann':
     fname= f'logT{np.log10(total_time):.1f}_N{number_particles}_logM{np.log10(particle_mass):.1f}_logTemp{np.log10(Temperature):.1f}_logSigma{np.log10(sigma_pos):.1f}.gif'
-
+# fname = 'test.gif'
 # Visualize the particle motion in 3D
-visualization(position_matrix, lim_bound=(-boundary_size/2, boundary_size/2), savegif=True, fname=fname)  # Call the visualization function to animate the particle motion
+visualization(position_matrix, mass, lim_bound=(-boundary_size/2, boundary_size/2), window_size=30, savegif=True, fname=fname)  # Call the visualization function to animate the particle motion
 
 # for s in range(step):   # Iterate through each time step
 #     # Store current velocity, acceleration, and position for each particle at time step s
